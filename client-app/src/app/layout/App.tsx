1ff4fdd7016e9ef285b6e812fe0ac5b3c4523ee6
Doug Herman <douglash9882@gmail.com>
2022-03-24 00:36:38 -0500

    finished routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,7 +4,7 @@ import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import HomePage from '../../features/home/HomePage';
 import { observer } from 'mobx-react-lite';
-import { Route } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
@@ -12,17 +12,26 @@ import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
 
-    return (
+  const location = useLocation();
+
+  return (
       <>
-        <NavBar />
-        <Container style={{marginTop: "7em"}}>
-          <Route exact path="/" component={HomePage}/>
-          <Route exact path="/activities" component={ActivityDashboard}/>
-          <Route exact path="/activities/:id" component={ActivityDetails}/>
-          <Route exact path="/createActivity" component={ActivityForm}/>
-        </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: "7em" }}>
+              <Route exact path="/activities" component={ActivityDashboard} />
+              <Route exact path="/activities/:id" component={ActivityDetails} />
+              <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityForm} />
+            </Container>
+          </>
+        )}
+        />
       </>
     );
   }
 
-export default observer(App);
+      export default observer(App);
