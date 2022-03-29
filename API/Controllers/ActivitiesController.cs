1b7c214a19e3382be0c60088e816ae89f2d89aad
Doug Herman <douglash9882@gmail.com>
2022-03-28 22:34:42 -0500

    more validations

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,25 +11,21 @@ namespace API.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
+        public async Task<IActionResult> GetActivities()
         {
-            return await Mediator.Send(new List.Query());
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")] // activities/id
         public async Task<IActionResult> GetActivity(Guid id)
         {
-            var result = await Mediator.Send(new Details.Query{Id = id});
-
-            if (result.IsSuccess && result.Value != null) return Ok(result.Value);
-            if (result.IsSuccess && result.Value == null) return NotFound();
-            return BadRequest(result.Error);
+            return HandleResult(await Mediator.Send(new Details.Query{Id = id}));
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+            return HandleResult(await Mediator.Send(new Create.Command {Activity = activity}));
         }
 
         [HttpPut("{id}")]
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -2,12 +2,13 @@ using Domain;
 using MediatR;
 using Persistence;
 using FluentValidation;
+using Application.Core;
 
 namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
@@ -20,7 +21,7 @@ namespace Application.Activities
             }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -29,13 +30,14 @@ namespace Application.Activities
                 
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 _context.Activities.Add(request.Activity);
 
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync() > 0;
+                if (!result) return Result<Unit>.Failure("Failed to add book...");
 
-                return Unit.Value;
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
