aae640ff005d7aead459828f96311c3b2791be3c
Doug Herman <douglash9882@gmail.com>
2022-04-05 19:08:07 -0500

    asdf

--- client-app/src/errors/ValidationErrors.tsx
+++ client-app/src/errors/ValidationErrors.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Message } from "semantic-ui-react";
 
 interface Props {
-    errors: string[];
+    errors: string[] | null;
 }
 
 export default function ValidationErrors({errors}: Props){
