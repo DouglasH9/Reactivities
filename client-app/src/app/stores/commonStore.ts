39d0101deb4ef6267b59e229d6cd6dceea5addaf
Doug Herman <douglash9882@gmail.com>
2022-04-06 18:42:43 -0500

    500 errors

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -2,7 +2,7 @@ import { makeAutoObservable } from "mobx";
 import { ServerError } from "../models/serverError";
 
 export default class CommonStore {
-    error!: ServerError | null;
+    error: ServerError | null = null;
 
     constructor() {
         makeAutoObservable(this);
