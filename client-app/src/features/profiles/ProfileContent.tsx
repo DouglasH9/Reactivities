9595cb73882184dd0b925ccb68233e34191f1d13
Doug Herman <douglash9882@gmail.com>
2022-04-25 22:46:25 -0500

    profile page updates

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Tab } from "semantic-ui-react";
 
 export default function ProfileContent() {
-    
+    const panes = [
+        {menuItem: "About", render: () => <Tab.Pane>About Content</Tab.Pane>},
+        {menuItem: "Photos", render: () => <Tab.Pane>Photo Content</Tab.Pane>},
+        {menuItem: "Books", render: () => <Tab.Pane>Books Content</Tab.Pane>},
+        {menuItem: "Followers", render: () => <Tab.Pane>Followers Content</Tab.Pane>},
+        {menuItem: "Following", render: () => <Tab.Pane>Following Content</Tab.Pane>},
+    ]
+
+    return (
+        <Tab
+            menu={{fluid: true, vertical: true}}
+            menuPosition="right"
+            panes={panes}
+        />
+    )
 }
