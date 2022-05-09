4a1e66882eb3ebd5d9958b85a144213a59495049
Doug Herman <douglash9882@gmail.com>
2022-05-09 18:17:48 -0500

    asdf

--- client-app/src/features/profiles/ProfileHeader.tsx
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -2,6 +2,7 @@ import { observer } from "mobx-react-lite";
 import React from "react";
 import { Button, Divider, Grid, Header, Item, ItemGroup, Reveal, Segment, Statistic } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
+import FollowButton from "./FollowButton";
 
 interface Props {
     profile: Profile;
@@ -27,19 +28,7 @@ export default observer (function ProfileHeader({profile}: Props) {
                         <Statistic label="Following" value={profile.followingCount} />
                     </Statistic.Group>
                     <Divider />
-                    <Reveal animated="move">
-                        <Reveal.Content visible style={{width: "100%"}}>
-                            <Button fluid color="purple" content="Following" />
-                        </Reveal.Content>
-                        <Reveal.Content hidden style={{width: "100%"}}>
-                            <Button 
-                                fluid
-                                basic
-                                color={true ? "pink" : "purple"}
-                                content={true ? "Unfollow" : "Follow"}
-                            />
-                        </Reveal.Content>
-                    </Reveal>
+                    <FollowButton profile={profile} />
                 </Grid.Column>
             </Grid>
         </Segment>
