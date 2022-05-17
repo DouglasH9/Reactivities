60f97ee0caa6a64c5313437e7815510740f096c0
Doug Herman <douglash9882@gmail.com>
2022-05-16 20:06:14 -0500

    added profile books display functionality

--- Application/Profiles/UserActivityDto.cs
+++ Application/Profiles/UserActivityDto.cs
@@ -12,8 +12,8 @@ namespace Application.Profiles
         public string Title { get; set; }
         public string Category { get; set; }
         public DateTime Date { get; set; }
-        
+
         [JsonIgnore]
-        public string HostUserName { get; set; }
+        public string HostUsername { get; set; }
     }
 }
